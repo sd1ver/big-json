cce8f7911c47f88c0f5a674a34d8804732a0b40c
Sergey <skazancev@gmail.com>
2020-09-03 22:53:32 +0300

    reformat code

--- src/main/scala/ru/github/sd1ver/JsonParser.scala
+++ src/main/scala/ru/github/sd1ver/JsonParser.scala
@@ -7,38 +7,38 @@ object JsonParser extends App {
 
   private val JsonStart = '{'
   private val JsonEnd = '}'
+
   implicit val formats: DefaultFormats.type = DefaultFormats
+
   case class Item(a: Int, b: Int)
-
   case class Answer(a: Int, b: Int, sum: Int)
-
-  val items = LazyList[Item]()
 
   val data     = """[ { "a": 1, "b": 2}, { "a": 2, "b":3}, { "a": 5, "b": 7 }]"""
   val lazyData = data.to(LazyList)
+  val answers = readAndCreateAnswer(lazyData)
+  println(answers.toList.mkString("\n"))
 
-  private def reader(stream: LazyList[Char]): LazyList[Answer] = {
+  private def readAndCreateAnswer(stream: LazyList[Char]): LazyList[Answer] = {
     val jsonStart = stream.dropWhile(c => c != JsonStart)
     stream.headOption.map { _ =>
       val jsonItem   = jsonStart.takeWhile(c => c != JsonEnd).toList :+ JsonEnd
       val jsonString = jsonItem.mkString
       val tailStream = jsonStart.tail.dropWhile(c => c != JsonStart)
       val answer     = jsonItemToAnswer(jsonString)
-      answer #:: reader(tailStream)
+      answer #:: readAndCreateAnswer(tailStream)
     }.getOrElse(LazyList.empty)
   }
 
-  def toAnswer(item: Item): Answer = {
+  private def toAnswer(item: Item): Answer = {
     val sum = item.a + item.b
     Answer(item.a, item.b, sum)
   }
 
-  def jsonItemToAnswer(json: String): Answer = {
+  private def jsonItemToAnswer(json: String): Answer = {
     val item = read[Item](json)
     toAnswer(item)
   }
 
-  val res = reader(lazyData)
-  println(res.toList.mkString("\n"))
+
 
 }
